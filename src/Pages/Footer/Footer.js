76e83132487a92579e8a76af37c176afefed1fd2
faisalcsebd <farhana.cse.bd@gmail.com>
2021-10-19 14:29:59 +0600

    nav done

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>THis is footer</h2>
+        // footer start 
+        <div className="bg-custom text-light text-center pt-5 cusFooter">
+            <Container className="footerGrid">
+                <div>
+                    <h3 className="text-start">Use full links</h3>
+                    <ul className="d-block p-0 m-0">
+                        <li>You can find us in online</li>
+                        <li>Contact with us</li>
+                        <li>[ Hotline - 10633 ]</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="text-start">QUICK LINKS</h3>
+                    <ul className="d-block p-0 m-0">
+                        <li>News and Medias</li>
+                        <li>Facilities</li>
+                        <li>MRD SERVICES</li>
+
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-start">GET US</h3>
+                    <ul className="d-block p-0 m-0">
+                        <li> 21 Shyamoli, Mirpur Road, Dhaka-1207, Bangladesh</li>
+
+
+                    </ul>
+                </div>
+            </Container>
+            <p className="p-3 m-0">Copyright © 2021. All Rights Reserved by Farhana Binte Hasan. </p>
         </div>
+
+        // footer end 
     );
 };
 
--- src/Pages/Home/Shared/Header/Header.js
+++ src/Pages/Home/Shared/Header/Header.js
@@ -1,10 +1,32 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>ONline Services</h2>
-        </div>
+        // nav bar start 
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#">Hot-Line</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="ms-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <NavLink className="text-decoration-none mx-3 text-light" to="/home">Home</NavLink>
+                        <NavLink className="text-decoration-none mx-3 text-light" to="/home#services">Services</NavLink>
+                        <NavLink className="text-decoration-none mx-3 text-light" to="/home#doctors">Doctors</NavLink>
+                        <NavLink className="text-decoration-none mx-3 text-light" to="/about">About Us</NavLink>
+
+                    </Nav>
+                    <Button className="mx-1" variant="btn btn-danger fw-bold">Login</Button>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        // nav bar end 
     );
 };
 
