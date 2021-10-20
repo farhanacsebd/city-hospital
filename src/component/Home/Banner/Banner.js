95358f530d227c83bc03bf96ec898bf794877b58
faisalcsebd <farhana.cse.bd@gmail.com>
2021-10-20 20:58:03 +0600

    banner

--- src/component/Home/Banner/Banner.js
+++ src/component/Home/Banner/Banner.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../image/banner/banner1.jpg';
-import banner2 from "../../../image/banner/banner2.jpg";
-import banner3 from "../../../image/banner/banner4.jpg";
+import banner1 from '../../../image/banner/banner2.jpg';
+import banner2 from "../../../image/banner/banner4.jpg";
+import banner3 from "../../../image/banner/banner5.jpg";
+import banner4 from "../../../image/banner/banner3.jpg";
+import banner5 from "../../../image/banner/banner1.jpg";
 
 const Banner = () => {
   return (
@@ -26,6 +28,20 @@ const Banner = () => {
           <img style={{ height: "500px" }}
             className="d-block w-100"
             src={banner3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img style={{ height: "500px" }}
+            className="d-block w-100"
+            src={banner4}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img style={{ height: "500px" }}
+            className="d-block w-100"
+            src={banner5}
             alt="Third slide"
           />
         </Carousel.Item>
--- src/component/Home/Home/Doctors/Doctors.js
+++ src/component/Home/Home/Doctors/Doctors.js
@@ -12,7 +12,8 @@ const Doctors = () => {
     return (
         <div className="py-5">
             <Container>
-                <h1 style={{ fontSize: "3rem" }} className="text-center text-info pb-3">OUR SERVICES & FACILITIES</h1>
+                <h1 style={{ fontSize: "3rem" }} className="text-center text-info pb-3">Most Provided Services</h1>
+                <p className="container text-body mb-5">To serve the humanity as a whole with this nobel vision.The trust has agreed upon to provide  health care service to the people with affordable cost.City Hospital has all the characteristics of a world-class hospital with wide range of services and specialists, equipments and technology, ambience and service quality.</p>
                 <Row xs={1} md={3} className="g-4">
                     {
                         doctors.map(doctor => <Doctor key={doctor.id} doctor={doctor}></Doctor>)
